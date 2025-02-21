c79e884ae564645ef3581fd0294651c8b8be7e5f
Garipally Mokshith <98528648+Mokshithg@users.noreply.github.com>
2025-02-21 12:41:23 +0530

    final commit after changing stop_container

--- stop_container.sh
+++ stop_container.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 set -e
 
-# Stop the running container (if any)
-echo "Hi"
+# Stop the running container (if it exists)
+echo "Stopping existing container..."
+docker stop sample-python-flask-project || true
+docker rm sample-python-flask-project || true
+
+echo "Container stopped successfully."
